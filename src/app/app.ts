87ba392d2935d927132e94e8e8d1e2e769c60b98
Nico Velasco <nicolasvelasco.r@gmail.com>
2025-11-20 12:42:05 -0300

    new components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Units } from './units/units';
+import { SearchBar } from './search-bar/search-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Units, SearchBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/today-forecast/today-forecast.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-today-forecast',
+  imports: [],
+  templateUrl: './today-forecast.html',
+  styleUrl: './today-forecast.css',
+})
+export class TodayForecast {
+
+}
